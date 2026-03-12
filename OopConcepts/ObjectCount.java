75f041017b5360e7711b1bc70fb4f69dcacc6e27
CodeWithKashvi <kashvijain2910@gmail.com>
2026-03-12 12:34:39 +0530

    added to count object in java

---
+++ OopConcepts/ObjectCount.java
@@ -0,0 +1,26 @@
+package OopConcepts;
+// Aim: Program to find number of objects created out of a class using static modifier.
+
+import java.util.Scanner;
+
+class Counter {
+    static int count = 0;
+
+    Counter() {
+        count++;
+    }
+}
+
+public class ObjectCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            new Counter();
+        }
+
+        System.out.println("Number of objects created: " + Counter.count);
+    }
+}
+
